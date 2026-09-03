06d8442b46c366366829a8219128d1a95d246a14
Brian <briankuria7@gmail.com>
2026-09-03 23:15:29 +0300

    new updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import DashboardPage from './Dashboard/page'
+import LoginPage from "./auth/login/page"
 
 export default function Home() {
-  return <DashboardPage />
+  return < LoginPage />
 }
